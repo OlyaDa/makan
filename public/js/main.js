4823cc64740b403829df89d82837a9e9e74fc37c
ola <dashevska.olga@gmail.com>
2020-02-07 10:36:43 +0200

    add select

--- public/js/main.js
+++ public/js/main.js
@@ -205,6 +205,7 @@ $(document).ready(function() {
 ////select
 
 var x, i, j, selElmnt, a, b, c;
+var iModels = 1;
 /* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
@@ -222,26 +223,59 @@ for (i = 0; i < x.length; i++) {
         create a new DIV that will act as an option item: */
         c = document.createElement("DIV");
         c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            var y, i, k, s, h;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < s.length; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    for (k = 0; k < y.length; k++) {
-                        y[k].removeAttribute("class");
+        if (i == iModels) {
+            c.addEventListener("click", function(e) {
+                /* When an item is clicked, update the original select box,
+                and the selected item: */
+                var y, i, k, s, h;
+                var myI = 0;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < s.length; i++) {
+                    console.log("avtoOnChange selected " +i+","+s.options[i].innerHTML+  this.innerHTML);
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        myI = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        for (k = 0; k < y.length; k++) {
+                            y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
                     }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
                 }
-            }
-            h.click();
-        });
+                h.click();
+            });
+        }
+        else {
+            c.addEventListener("click", function(e) {
+                console.log("avtoOnChange selected " +  this.innerHTML);
+                /* When an item is clicked, update the original select box,
+                and the selected item: */
+                var y, i, k, s, h;
+                var myI = 0;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < s.length; i++) {
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        console.log("avtoOnChange selected " + i + "," + typeof (s));
+                        myI = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        for (k = 0; k < y.length; k++) {
+                            y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
+                    }
+                }
+                setModel(myI);
+                h.click();
+            });
+        }
+
         b.appendChild(c);
     }
     x[i].appendChild(b);
@@ -278,3 +312,86 @@ function closeAllSelect(elmnt) {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
+
+let carModelNo = "Модель автомобиля";
+
+let carModels = {
+    "1":["Accent","Aslan","Atoz","Avante","Click","Equus","Galloper","Genesis","Grace","Grandeur","loniq","Kona","Maxcruz","SantaFe","Solati","Sonata","Starex","Terracan","Trajet XG","Tucson","Tuscani","Veloster","Veracruz","Verna","i30","i40"],
+    "2":["Bongo 3 Minibus","Carens","Carnival","Cerato","Forte","K3","K5","K7","K9","Lotze","Mohave","Morning","Niro","Opirus","Pregio","Pride","Ray","Retona","Sorento","Soul","Sportage","Stinger","Stonic","Visto","X TREK"],
+    "3":["Alpheon","Aveo","Camaro","Captiva","Cruze", "Damas","Gentra","Lacetti","Magnuss","Malibu","Matiz","Orlando","Spark", "Toska","Trax","Winstorm"],
+    "4":["QM3","QM5","QM6","SM3","SM5","SM6","SM7"],
+    "5":["Actyon","Istana5","Korando","Kyron","Musso","Rexton","Rodius","Tivoli"],
+    "6":["A4","A5","A6","A8","Alloroad Quattro","Q3","Q5","Q7","TT"],
+    "7":["1 Series","2 Series","3 Series","4 Series","5 Series","6 Series","7 Series","Grand Turismo","M","X1","X3","X5","X6","Z4"],
+    "8":["Escalade","STS"],
+    "9":["Model Detail"],
+    "10":["200","300c","Grand Voyager","Sebring"],
+    "11":["DS3"],
+    "12":["Copen"],
+    "13":["Avenger","Challenger","Dakota"],
+    "14":["Explorer","Five Hundred","Kuga","Mondeo","Taurus"],
+    "15":["Accord","CR V","Civic","Legend","Prelude"],
+    "16":["FX","G","M","Q","QX","Jaguar","S Type"],
+    "17":["Cherokee","Compass","Patriot","Renegade","Wrangler"],
+    "18":["Discovery","Discovery Sport","Freelander"],
+    "19":["CT200h","ES","IS","LS","NX","RX"],
+    "20":["MKS"],
+    "21":["B Class","C Class","CLS Class","E Class","GLA Class","M Class","S Class"],
+    "22":["Coupe"],
+    "23":["Lancer","Qutlander","RVR"],
+    "24":["370Z","Altima","Cube","Juke","Qashqui"],
+    "25":["207","307","508"],
+    "26":["Cayenne"],
+    "27":["Camry","Prius","RAV4","Voxy"],
+    "28":["Beetle","CC","Golf","Jetta","Passat","Phaeton","Polo","Scirocco","Tiguan"],
+    "29":["C30","S60","S80","V40","XC60","XC70","XC90"],
+    "30":["G80"],
+    "31":["Model Detail"],
+}
+
+function setModel(carIndex)
+{
+    console.log("setModel=" + carIndex);
+    if (carIndex == 0) return;
+    let selectElement = document.getElementById("carModel");
+
+    let divClassSeleted = selectElement.nextElementSibling;
+    divClassSeleted.innerHTML = carModelNo;
+
+    let divSelected = selectElement.nextElementSibling.nextElementSibling;
+    divSelected.innerHTML = '';
+
+    let models = carModels[carIndex];
+
+    for (i = 0; i < models.length; i++) {
+        var div = document.createElement('div');
+        div.innerHTML = models[i];
+        divSelected.appendChild(div);
+        div.addEventListener("click", function(e) {
+            /* When an item is clicked, update the original select box,
+            and the selected item: */
+            var y, i, k, s, h;
+            var myI = 0;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < s.length; i++) {
+                console.log("avtoOnChange selected " +i+","+s.options[i].innerHTML+  this.innerHTML);
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    myI = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    for (k = 0; k < y.length; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+    }
+
+
+}
+
